588284d1b25a4e2f7d9cd65e4ff4c39309d5b94e
evaldoe397 <evaldoe397@gmail.com>
2025-02-24 14:31:02 -0300

    Revert "Criacao dos relatorios de ranking e posicao dos indicadores"
    This reverts commit 34836576264c43c21b0ae0e6175e7e8ea8887b3b.
    
    :wq

--- src/main/java/br/com/nlw/events/controller/SubscriptionController.java
+++ src/main/java/br/com/nlw/events/controller/SubscriptionController.java
@@ -2,7 +2,6 @@ package br.com.nlw.events.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -44,28 +43,4 @@ public class SubscriptionController {
 		
 		return ResponseEntity.badRequest().build();
 	}
-	
-	@GetMapping("/subscription/{prettyName}/ranking")
-	public ResponseEntity<?> generateRankingByEvent(@PathVariable String prettyName){
-		try {
-			return ResponseEntity.ok(service.getCompleteRanking(prettyName).subList(0, 3));
-		}
-		catch(EventNotFoundException e) {
-			return ResponseEntity.status(404).body(new ErrorMessage(e.getMessage()));
-		}
-	}
-	
-	@GetMapping("/subscription/{prettyName}/ranking/{userId}")
-	public ResponseEntity<?> generateRankingByEventAndUser(@PathVariable String prettyName,
-			@PathVariable Integer userId){
-		
-		try {
-			return ResponseEntity.ok(service.getRankingByUser(prettyName, userId));
-		}
-		catch(Exception e) {
-			return ResponseEntity.status(404).body(new ErrorMessage(e.getMessage()));
-		}
-		
-	}
-	
 }
--- src/main/java/br/com/nlw/events/dto/SubscriptionRankingItem.java
+++
@@ -1,5 +0,0 @@
-package br.com.nlw.events.dto;
-
-public record SubscriptionRankingItem(Long subscribers, Integer user_id, String name) {
-	
-}
